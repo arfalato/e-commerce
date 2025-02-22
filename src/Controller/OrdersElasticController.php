9dded1c4970131ec51b24f8bebd505db0851b138
Angelo Raffaele Falato <rfalato@gmail.com>
2025-02-22 01:36:10 +0100

    improved Elastic results

--- src/Controller/OrdersElasticController.php
+++ src/Controller/OrdersElasticController.php
@@ -1,9 +1,11 @@
 <?php
 
-
 namespace App\Controller;
 
+use Exception;
 use FOS\ElasticaBundle\Finder\PaginatedFinderInterface;
+use Elastica\Query\BoolQuery;
+use Elastica\Query\MultiMatch;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -21,17 +23,15 @@ class OrdersElasticController extends AbstractController
     #[Route('/orders-elastic', methods: ['GET'])]
     public function searchOrders(Request $request): Response
     {
-
         $query = $request->query->get('q', '');
         $page = $request->query->get('page', 1);
         $limit = $request->query->get('limit', 10);
 
 
-        $boolQuery = new \Elastica\Query\BoolQuery();
+        $boolQuery = new BoolQuery();
 
         if (!empty($query)) {
-
-            $multiMatch = new \Elastica\Query\MultiMatch();
+            $multiMatch = new MultiMatch();
             $multiMatch->setQuery($query);
             $multiMatch->setFields([
                 'id^3',
@@ -54,14 +54,24 @@ class OrdersElasticController extends AbstractController
                 'orders' => array_map(function ($order) {
                     return [
                         'id' => $order->getId(),
+                        'date' => $order->getCreationDate()->format('Y-m-d H:i:s'),
                         'name' => $order->getName(),
                         'description' => $order->getDescription(),
+                        'totalAmount' => $order->getTotalAmount(),
+                        'products' => array_map(function ($item) {
+                            return [
+                                'id' => $item->getProduct()->getId(),
+                                'name' => $item->getProduct()->getName(),
+                                'quantity' => $item->getQuantity(),
+                                'price' => (float)$item->getProduct()->getPrice(),
+                            ];
+                        }, $order->getOrderItems()->toArray())
                     ];
                 }, $paginator->getCurrentPageResults())
             ];
 
             return $this->json($results);
-        } catch (\Exception $e) {
+        } catch (Exception $e) {
             return $this->json([
                 'error' => 'Search error occurred',
                 'message' => $e->getMessage()
